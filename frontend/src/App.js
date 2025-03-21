cf856b333fb70464f407649085465ce6d1e3033e
Arthurosus <arthuralbpenmou@gmail.com>
2025-03-21 16:37:39 -0300

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CompanyRegistration from "./pages/CompanyRegistration";
-import CompanyList from "./pages/CompanyList";  // Importa a página de listagem
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/">Cadastro</Link> |  
-          <Link to="/empresas"> Lista de Empresas</Link>
-        </nav>
         <Routes>
           <Route path="/" element={<CompanyRegistration />} />
-          <Route path="/empresas" element={<CompanyList />} />  
         </Routes>
       </div>
     </Router>
